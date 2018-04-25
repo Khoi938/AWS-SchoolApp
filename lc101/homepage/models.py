bcdf5f12a9e974436d50787342abfbc7f853d143
EC2 Default User <ec2-user@ip-10-1-1-62.us-west-2.compute.internal>
2018-04-25 11:30:33 +0000

    built model

--- lc101/homepage/models.py
+++ lc101/homepage/models.py
@@ -4,14 +4,30 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    school_id = models.IntegerField(max_length=10)
+    school_id = models.IntegerField()
     is_student = models.BooleanField('Student',default=False)
     is_teacher = models.BooleanField('Teacher',default=False)
     
-class Math():
+class Student(models.Model):
+    student = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    class1 = models.ForeignKey('Subject', null=True, on_delete = models.SET_NULL, related_name='class1')
+    class2 = models.ForeignKey('Subject', null=True, on_delete = models.SET_NULL, related_name='class2')
+    class3 = models.ForeignKey('Subject', null=True, on_delete = models.SET_NULL, related_name='class3')
+    class3 = models.ForeignKey('Subject', null=True, on_delete = models.SET_NULL, related_name='class4')
+    
+class Teacher(models.Model):
+    teacher = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    teaches1 = models.ForeignKey('Subject', null=True, on_delete = models.SET_NULL, related_name='teaches1')
+    teaches2 = models.ForeignKey('Subject', null=True, on_delete = models.SET_NULL, related_name='teaches2')
+    teaches3 = models.ForeignKey('Subject', null=True, on_delete = models.SET_NULL, related_name='teaches3')
+    teaches4 = models.ForeignKey('Subject', null=True, on_delete = models.SET_NULL, related_name='teaches4')
+    
+class Subject(models.Model):
+    subject = models.CharField(max_length=30)
     monday = models.CharField(max_length=300)
-    Tuesday = models.CharField(max_length=300)
-    monday = models.CharField(max_length=300)
-    monday = models.CharField(max_length=300)
-    monday = models.CharField(max_length=300)
+    tuesday = models.CharField(max_length=300)
+    wednesday = models.CharField(max_length=300)
+    thursday = models.CharField(max_length=300)
+    friday = models.CharField(max_length=300)
+    
     
