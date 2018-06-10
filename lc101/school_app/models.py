5f30455c93d68a1f54d06554b7115903fb72057b
Khoi Le <eclipse938@gmail.com>
2018-06-10 11:35:50 +0000

    working on model

--- lc101/school_app/models.py
+++ lc101/school_app/models.py
@@ -11,13 +11,13 @@ class Profile(models.Model):
     address = models.CharField(max_length=200, blank=True)
     
     birth_date = models.DateField(null=True, blank=True)
-    school_id = models.IntegerField(default=0)
+    school_id = models.IntegerField(null=True, blank = True, default=0)
     
     is_student = models.BooleanField('Student', default=False)
     is_teacher = models.BooleanField('Teacher', default=False)
     def __str__(self):#Return the string representation of the object
         return '%s %s'%(self.user.first_name, self.user.last_name)
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=User)# Create a Profile when a User is create.
 def create_profile_object(sender, instance, created, **kwargs):
     if created:
         profile = Profile.objects.create(user=instance)
@@ -25,32 +25,30 @@ def create_profile_object(sender, instance, created, **kwargs):
     
         
 class Student(models.Model):
-    student_profile = models.OneToOneField(Profile, null=True, on_delete = models.SET_NULL)
+    profile = models.OneToOneField(Profile, null=True, on_delete = models.SET_NULL)
     user = models.OneToOneField(User, null=True, on_delete = models.SET_NULL)
-    # class1 = models.ForeignKey('Algebra_101',blank=True, null=True, on_delete = models.SET_NULL, related_name='class1')
-    # class2 = models.ForeignKey('Biology_101',blank=True, null=True, on_delete = models.SET_NULL, related_name='class2')
-    # class3 = models.ForeignKey('Subject',blank=True, null=True, on_delete = models.SET_NULL, related_name='class3')
-    # class3 = models.ForeignKey('Subject',blank=True, null=True, on_delete = models.SET_NULL, related_name='class4')
+    subject_taking = models.ForeignKey('Subject',blank=True, null=True, on_delete = models.SET_NULL, related_name='enrolled_subject')
+    classroom = models.ManyToManyField('Classroom')
     def __str__(self):
-        return str(self.user.get_full_name()) #change to string
+        return 'Student: ' + str(self.user.get_full_name()) #change to string
         
 class Teacher(models.Model):
-    teacher_profile = models.OneToOneField(Profile,null=True, on_delete = models.SET_NULL)
+    profile = models.OneToOneField(Profile,null=True, on_delete = models.SET_NULL)
     user = models.OneToOneField(User, null=True, on_delete = models.SET_NULL)
-    # teaches1 = models.ForeignKey('Algebra_101',blank=True, null=True, on_delete = models.SET_NULL, related_name='teaches1')
-    # teaches2 = models.ForeignKey('Biology_101',blank=True, null=True, on_delete = models.SET_NULL, related_name='teaches2')
-    teaches = models.ForeignKey('Subject',blank=True, null=True, on_delete = models.SET_NULL, related_name='teaches')
-    # teaches4 = models.ForeignKey('Subject',blank=True, null=True, on_delete = models.SET_NULL, related_name='teaches4')
+    teaches = models.ForeignKey('Subject',blank=True, null=True, on_delete = models.SET_NULL, related_name='teaches_subject')
     def __str__(self):
-        return str(self.user.get_full_name()) #change to string
+        return 'Teacher: ' + str(self.user.get_full_name()) #change to string
         
 # --------------- Subject, Classroom, Department's Model----------------------
-class Subject(models.Model):
-    teacher = models.ManyToManyField(Teacher)
-    name = models.CharField(max_length=150,default='')
+class Subject(models.Model): #Reverse Look up Subject.objects.filter(teacher_subject = subject.name)
+    name = models.CharField(max_length=150,default='', unique=True)
     description = models.CharField(max_length=450,default='')
-    classroom = models.ForeignKey('Classroom', blank=True,null=True, on_delete = models.SET_NULL)
+    semester = models.CharField(max_length=50,default='')
+    year = models.CharField(max_length=4,default='')
     
+    classroom = models.ForeignKey('Classroom', blank=True,null=True, on_delete = models.SET_NULL)
+# A Subject is bound to the teacher and create a classom upon creation. More room can be added later
+
     weekly_agenda = models.CharField(max_length=450,default='Agenda Goes Here')
     last_modifield = models.DateTimeField(auto_now_add=True, blank=True)
    
@@ -73,18 +71,18 @@ class Subject(models.Model):
 
         
 class Classroom(models.Model):
-    description = models.CharField(max_length=450,default='')
-    name = models.CharField(max_length=50,default='')
-    
-    semester = models.CharField(max_length=50,default='')
-    room_number = models.CharField(max_length=50,default='')
-    year = models.CharField(max_length=5,default='')
-    
-    teacher = models.OneToOneField(Teacher, null=True, on_delete = models.SET_NULL)
+    room_number = models.CharField(max_length=10,default='')
+    subject_name= models.CharField(max_length=50,default='')
+    teacher = models.ManyToManyField(Subject, blank=True)# Use for statement to get value
     subject = models.ManyToManyField(Subject, blank=True)
     student = models.ManyToManyField(Student, blank=True)
     def __str__(self):
-        return 'Subject: ' + self.subject.name + self.description
+        return 'Subject: ' + self.subject.name 
+# To Find the Classroom:
+# Classroom.objects.filter(player__name__in=['Player1','Player2'])
+# teacher = Teacher.objects.filter(user=request.user)
+# subject = Subject.objects.filter(teacher_set=teacher, name ='subject_name')
+# classroom = Classroom.objects.filter(teacher=teacher).filter(subject=subject)
 @receiver(post_save, sender=Subject)
 def create_classroom_object(sender, instance, created, **kwargs):
     if created:
--- lc101/school_app/views_functions.py
+++ lc101/school_app/views_functions.py
@@ -23,3 +23,11 @@ def is_teacher(request):
         return False
     return True
     
+# Add Class to Student and Check number of current class
+def add_class(request, student, subject):
+        if student.enrolled.count() >= 7:#Count hit DB return with num, len(s) pull the entire list out and count it.
+            messages.warning(request,'Student cannot be enrolled in more than 7 Class')
+            return False
+        else:
+            student.enrolled.add(subject)
+            return True
